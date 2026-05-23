00456372c5596b5f0b1421cc53157fda7f6b3e8a
Viktor Volokh <volohviktor1@gmail.com>
2026-05-24 02:28:41 +0300

    feat: implement Gauss-Jordan elimination algorithm
    - add MatrixSubstract logic for row operations
    - integrate full Gauss-Jordan solving pipeline

--- Gauss-Jordan algorithm/Program.cs
+++ Gauss-Jordan algorithm/Program.cs
@@ -14,12 +14,28 @@ namespace Gauss_Jordan_algorithm
 
         public static void GaussSolving(double[][] matrix)
         {
-            MatrixDivide(matrix, 0, 0);
-            for (int i = 1; i < matrix.Length; i++)
+            for (int i = 0; i < matrix.Length; i++)
             {
-                MatrixSubstract(matrix, i, 0, 0);
+                MatrixDivide(matrix, i, i);
+                if (i != matrix.Length - 1)
+                {
+                    for (int j = i + 1; j < matrix.Length; j++)
+                    {
+                        MatrixSubstract(matrix, j, i, i);
+                    }
+                }
             }
-            
+            for (int i = matrix.Length - 1; i > -1; i--)
+            {
+
+                for (int j = matrix.Length - 1; j > -1; j--)
+                {
+                    if (i != j)
+                    {
+                        MatrixSubstract(matrix, j, i, i);
+                    }
+                }      
+            }
         }
 
         public static void MatrixDivide(double[][] matrix, int row, int number)
@@ -33,20 +49,10 @@ namespace Gauss_Jordan_algorithm
 
         public static void MatrixSubstract(double[][] matrix, int mainRow, int secondaryRow, int target)
         {
-            while (matrix[mainRow][target] != 0)
+            double coefficient = matrix[mainRow][target];
+            for (int i = 0; i < matrix.Length + 1; i++)
             {
-                if (matrix[mainRow][target] > 0)
-                    for (int i = 0; i < matrix.Length; i++)
-                    {
-                        matrix[mainRow][i] = matrix[mainRow][i] - matrix[secondaryRow][i];
-                    }
-                else
-                {
-                    for (int i = 0; i < matrix.Length; i++)
-                    {
-                        matrix[mainRow][i] = matrix[mainRow][i] + matrix[secondaryRow][i];
-                    }
-                }
+                matrix[mainRow][i] = matrix[mainRow][i] - coefficient * matrix[secondaryRow][i];
             }
         }
 
@@ -64,12 +70,13 @@ namespace Gauss_Jordan_algorithm
         static void Main(string[] args)
         {
             double[][] matrix =
-            [
-                [4, 2, 3, 5],
-                [2, 6, 3, 4],
-                [3, 8, 8, 7]
-
-            ];
+[
+    [1,  1,  1,  1,  1,  15], // 1 + 2 + 3 + 4 + 5 = 15
+    [2, -1,  3,  1, -1,   8], // 2(1) - 2 + 3(3) + 4 - 5 = 9  (ТУТ КІНЦЕВА 9!)
+    [1,  3, -1,  2,  2,  22], // 1 + 3(2) - 3 + 2(4) + 2(5) = 22
+    [3,  1,  2, -1,  1,  12], // 3(1) + 2 + 2(3) - 4 + 5 = 12
+    [1,  2,  1, -3,  1,  1]
+];
 
             GaussSolving(matrix);
 
